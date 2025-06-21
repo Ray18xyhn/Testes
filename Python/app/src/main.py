2949abc1726b370a59b3b020a0245d6c82cb61e2
Ray <ray18xyhn@gmail.com>
2025-06-21 20:38:08 -0300

    main

--- Python/app/src/main.py
+++ Python/app/src/main.py
@@ -2,8 +2,9 @@ import flet as ft
 
 
 def main(page: ft.Page):
-    counter = ft.Text("0", size=50, data=0, color=ft.colors.BLACK)
-    page.bgcolor = ft.colors.RED
+    page.horizontal_alignment = 'center'
+    counter = ft.Text("0", size=50, data=0, color="WHIT", weight=ft.FontWeight.BOLD)
+    page.bgcolor = "WHITE"
 
     def increment_click(e):
         counter.data += 1
@@ -11,18 +12,25 @@ def main(page: ft.Page):
         counter.update()
 
     page.floating_action_button = ft.FloatingActionButton(
-        icon=ft.icons.ADD, on_click=increment_click
+        icon=ft.Icons.ADD, on_click=increment_click
     )
     page.add(
         ft.SafeArea(
             ft.Container(
                 counter,
                 alignment=ft.alignment.center,
+                bgcolor='BLUE',
+                border_radius=13,
+                shadow=ft.BoxShadow(
+                    color='BLACK54',
+                    blur_radius=14,
+                    offset=ft.Offset(0, 8)
+                    )
             ),
-            height=100,
-            width=100
-        )
+            height=600,
+            width=400,
+        ),
     )
 
 
-ft.app(main)
+ft.app(target=main, view=ft.AppView.WEB_BROWSER)
